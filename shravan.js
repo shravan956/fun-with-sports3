48a642e6060ed4469dac562dba90d1e969ab6887
shravan956 <shravan.jukar24@sakec.ac.in>
2024-12-03 12:08:54 +0530

    index file added
    index file added

---
+++ shravan.js
@@ -0,0 +1,15 @@
+document.write("<hr>"); 
+document.write("hello world wide web!"); 
+document.write("<hr>");
+    document.getElementById("demo").innerHTML=5+7; 
+    document.write(5+7);
+    window.alert(5+7);
+    var myvar= 31;
+    document.write(myvar);
+    var text="";
+    var i=1
+    while(i<10)
+    {
+        document.write("<br>The number is" +i);
+        i++;
+    }
